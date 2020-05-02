88f2e780394966c7ec504c4164eb2aad6ebf7555
noandasan <n_oandasan@yahoo.com>
2020-05-02 23:55:51 +0300

    u
    u

--- server.js
+++ server.js
@@ -26,6 +26,28 @@ app.get('/webhook/', function(req, res) {
     res.send('Error, wrong token')
 })
 
+app.post('/webhook/', function(req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            if (text === 'hi') {
+                sendGenericMessage(sender)
+                continue
+            }
+            sendTextMessage(sender, "parrot: " + text.substring(0, 200))
+        }
+        if (event.postback) {
+            text = JSON.stringify(event.postback)
+            sendTextMessage(sender, "Postback received: " + text.substring(0, 200), token)
+            continue
+        }
+    }
+    res.sendStatus(200)
+})
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
