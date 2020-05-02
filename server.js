4fe3d6344b865565c9a9406c71872ea760683d23
noandasan <n_oandasan@yahoo.com>
2020-05-02 21:11:09 +0300

    latest
    latest

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+
+app.get("/", (res, req) => {
+    req.send("fds");
+})
+
+app.get('/webhook/', function(req, res) {
+    if (req.query['hub.verify_token'] === 'bong') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
