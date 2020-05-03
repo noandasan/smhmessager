15cda129cc89c47216a5f4825badb0ebdeeadfb6
noandasan <n_oandasan@yahoo.com>
2020-05-03 23:55:57 +0300

    u
    u

--- server.js
+++ server.js
@@ -31,6 +31,9 @@ const
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
+app.get('/', (req, res) => {
+    res.send("bong");
+});
 // Accepts POST requests at /webhook endpoint
 app.post('/webhook', (req, res) => {
 
