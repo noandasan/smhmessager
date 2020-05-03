4fd6b7483902d40ff321c6d277988dafa46ea138
noandasan <n_oandasan@yahoo.com>
2020-05-03 11:48:14 +0300

    u
    u

--- server.js
+++ server.js
@@ -1,159 +1,64 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser');
+'use strict';
 
-app.use(bodyParser.urlencoded({ extended: false }))
+// Imports dependencies and set up http server
+const
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express().use(bodyParser.json()); // creates express http server
 
-// Process application/json
-app.use(bodyParser.json())
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-app.get("/", (res, req) => {
-    req.send("fds");
-})
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {
 
-app.get("/privacy/", (res, req) => {
-    req.send("privacy1");
-})
+    let body = req.body;
 
-app.get("/terms/", (res, req) => {
-    req.send("bongbong");
-})
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
 
-app.get('/webhook/', function(req, res) {
-    if (req.query['hub.verify_token'] === 'bong') {
-        res.send(req.query['hub.challenge'])
+        // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach(function(entry) {
+
+            // Gets the message. entry.messaging is an array, but 
+            // will only ever contain one message, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+        });
+
+        // Returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
     }
-    res.send('Error, wrong token')
-})
 
-app.post('/webhook/', function(req, res) {
-    messaging_events = req.body.entry[0].messaging
-    for (i = 0; i < messaging_events.length; i++) {
-        event = req.body.entry[0].messaging[i]
-        sender = event.sender.id
-        if (event.message && event.message.text) {
-            text = event.message.text
-            if (text === 'hi') {
-                sendGenericMessage(sender)
-                continue
-            }
-            sendTextMessage(sender, "parrot: " + text.substring(0, 200))
-        }
-        if (event.postback) {
-            text = JSON.stringify(event.postback)
-            sendTextMessage(sender, "Postback received: " + text.substring(0, 200), token)
-            continue
-        }
-    }
-    res.sendStatus(200)
-})
+});
 
-var token = "EAAJkaiWYIfYBABc98dsr9AxgWS0oZBqR9wVofziAyT7EfiUjKz8lVlcujD1KBbYT1K0XnKyMJnBrWcb8LlZBkNjaoBVDazSmiYNAHXmka02ZA4ALaFE1oZBuDArcOnhGTrCTm6zXztpa2CTtFa2ZC4VQwRf9mHVZByZBCrX3uUDNwZDZD"
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
 
-// function to echo back messages - added by Stefan
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "EAAJkaiWYIfYBAHMEhMrD7qLd3oKkuHLITKlSvE8Bzqr31qTdfz2JmsQLvwAXS103AcFWFADdna8mqlEWbrzCZCFZAjcHpCnLKOnTfrL4Tudx60Ncz1zI1ZCFTl9T7bZC1VV0maZAmDDlusxcs0gJTkd7BmmqtAZCHEJP56wZAZAVcwZDZD"
 
-function sendTextMessage(sender, text) {
-    messageData = {
-        text: text
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: token },
-        method: 'POST',
-        json: {
-            recipient: { id: sender },
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
 
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
 
-// Send an test message back as two cards.
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
 
-function sendGenericMessage(sender) {
-    messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "Ai Chat Bot Communities",
-                    "subtitle": "Communities to Follow",
-                    "image_url": "http://1u88jj3r4db2x4txp44yqfj1.wpengine.netdna-cdn.com/wp-content/uploads/2016/04/chatbot-930x659.jpg",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.facebook.com/groups/aichatbots/",
-                        "title": "FB Chatbot Group"
-                    }, {
-                        "type": "web_url",
-                        "url": "https://www.reddit.com/r/Chat_Bots/",
-                        "title": "Chatbots on Reddit"
-                    }, {
-                        "type": "web_url",
-                        "url": "https://twitter.com/aichatbots",
-                        "title": "Chatbots on Twitter"
-                    }],
-                }, {
-                    "title": "Chatbots FAQ",
-                    "subtitle": "Aking the Deep Questions",
-                    "image_url": "https://tctechcrunch2011.files.wordpress.com/2016/04/facebook-chatbots.png?w=738",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "What's the benefit?",
-                        "payload": "Chatbots make content interactive instead of static",
-                    }, {
-                        "type": "postback",
-                        "title": "What can Chatbots do",
-                        "payload": "One day Chatbots will control the Internet of Things! You will be able to control your homes temperature with a text",
-                    }, {
-                        "type": "postback",
-                        "title": "The Future",
-                        "payload": "Chatbots are fun! One day your BFF might be a Chatbot",
-                    }],
-                }, {
-                    "title": "Learning More",
-                    "subtitle": "Aking the Deep Questions",
-                    "image_url": "http://www.brandknewmag.com/wp-content/uploads/2015/12/cortana.jpg",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "AIML",
-                        "payload": "Checkout Artificial Intelligence Mark Up Language. Its easier than you think!",
-                    }, {
-                        "type": "postback",
-                        "title": "Machine Learning",
-                        "payload": "Use python to teach your maching in 16D space in 15min",
-                    }, {
-                        "type": "postback",
-                        "title": "Communities",
-                        "payload": "Online communities & Meetups are the best way to stay ahead of the curve!",
-                    }],
-                }]
-            }
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
         }
     }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: token },
-        method: 'POST',
-        json: {
-            recipient: { id: sender },
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
 });
